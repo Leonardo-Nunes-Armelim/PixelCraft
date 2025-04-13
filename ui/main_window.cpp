4aad59333637ce32d440b554213dd710b15b62c7
Leonardo-Nunes-Armelim <leonardo.nunes.armelim@gmail.com>
2025-04-13 17:39:06 -0300

    Removing dependency on main_window.ui

--- ui/main_window.cpp
+++ ui/main_window.cpp
@@ -1,5 +1,4 @@
 #include "main_window.h"
-#include "./ui_main_window.h"
 
 #include "../core/canvas_layer_item.h"
 #include "canvas_scene.h"
@@ -17,91 +16,127 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    this->resize(1280, 720);
+    //this->showMaximized();
 
-    setupCanvas();
+    // Central Widget
+    QWidget *central_widget = new QWidget(this);
+    this->setCentralWidget(central_widget);
 
-    //scene = new QGraphicsScene(this);
-    //view = new QGraphicsView(scene, this);
-    //view->setSceneRect(0, 0, 320, 240);
-    //view->setRenderHint(QPainter::Antialiasing, false);
-    //view->setDragMode(QGraphicsView::NoDrag);
+    // Configuring Central Widget
+    QVBoxLayout *layout = new QVBoxLayout(central_widget);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(0);
 
-    //setCentralWidget(view);
-    //resize(400, 300);
+    // Contextual tool bar
+    QWidget *context_tool_bar = new QWidget(this);
+    context_tool_bar->setFixedHeight(50);
+    context_tool_bar->setStyleSheet("background-color: gray;");
+
+    // Center left widget
+    QWidget *tools_and_canvas_parent_widget = new QWidget();
+    QHBoxLayout *tools_and_canvas_layout = new QHBoxLayout(tools_and_canvas_parent_widget);
+    QWidget *tool_bar = new QWidget(this);
+    tool_bar->setFixedWidth(100);
+    tool_bar->setStyleSheet("background-color: red;");
+    tools_and_canvas_layout->addWidget(tool_bar);
+
+    // Canvas
+    CanvasScene *canvas_scene = new CanvasScene(this);
+    CanvasView *canvas_view = new CanvasView(this);
+    canvas_view->setScene(canvas_scene);
+    canvas_view->setDragMode(QGraphicsView::NoDrag);
+    CanvasLayerItem *canvas_layer = new CanvasLayerItem(128, 128);
+    canvas_scene->addItem(canvas_layer);
+
+    connect(canvas_scene, &CanvasScene::pixelDrawn, this, [=](int x, int y) {
+        canvas_layer->drawPixel(x, y, Qt::black);
+    });
+
+    tools_and_canvas_layout->addWidget(canvas_view);
+
+    // Animations panel
+    QWidget *animations = new QWidget();
+    animations->setMinimumWidth(200);
+    animations->setMinimumHeight(200);
+    animations->setStyleSheet("background-color: green;");
+    //QPushButton *btn_1 = new QPushButton("btn 1");
+    //QPushButton *btn_2 = new QPushButton("btn 2");
+    //QPushButton *btn_3 = new QPushButton("btn 3");
+    //QVBoxLayout *leftLayout = new QVBoxLayout(leftPane);
+    //leftLayout->addWidget(btn_1);
+    //leftLayout->addWidget(btn_2);
+    //leftLayout->addWidget(btn_3);
+
+    // Frame Edit panel
+    QWidget *frame_edit = new QWidget();
+    frame_edit->setMinimumWidth(200);
+    frame_edit->setMinimumHeight(200);
+
+    // Playback panel
+    QWidget *playback = new QWidget();
+    playback->setMinimumWidth(200);
+    playback->setMinimumHeight(200);
+    playback->setStyleSheet("background-color: green;");
+
+    // Lower bar splitter
+    QSplitter *lower_bar_splitter = new QSplitter(Qt::Horizontal, this);
+    lower_bar_splitter->addWidget(animations);
+    lower_bar_splitter->addWidget(frame_edit);
+    lower_bar_splitter->addWidget(playback);
+    lower_bar_splitter->setCollapsible(0, false);
+    lower_bar_splitter->setCollapsible(1, false);
+    lower_bar_splitter->setCollapsible(3, false);
+
+    // Center splitter
+    QSplitter *center_splitter = new QSplitter(Qt::Vertical, this);
+    center_splitter->addWidget(tools_and_canvas_parent_widget);
+    center_splitter->addWidget(lower_bar_splitter);
+
+    // Right side panel splitter
+    QWidget *preview_panel = new QWidget();
+    preview_panel->setMinimumHeight(100);
+    preview_panel->setMinimumWidth(200);
+    preview_panel->setStyleSheet("background-color: red;");
+    QWidget *palette_panel = new QWidget();
+    palette_panel->setMinimumHeight(100);
+    palette_panel->setStyleSheet("background-color: green;");
+    QWidget *tiles_panel = new QWidget();
+    tiles_panel->setMinimumHeight(100);
+    tiles_panel->setStyleSheet("background-color: blue;");
+    QWidget *layers_panel = new QWidget();
+    layers_panel->setMinimumHeight(100);
+    layers_panel->setStyleSheet("background-color: red;");
+    QWidget *info_panel = new QWidget();
+    info_panel->setMinimumHeight(100);
+    info_panel->setStyleSheet("background-color: blue;");
+
+    QSplitter *right_side_panel_splitter = new QSplitter(Qt::Vertical, this);
+    right_side_panel_splitter->addWidget(preview_panel);
+    right_side_panel_splitter->addWidget(palette_panel);
+    right_side_panel_splitter->addWidget(tiles_panel);
+    right_side_panel_splitter->addWidget(layers_panel);
+    right_side_panel_splitter->addWidget(info_panel);
+    right_side_panel_splitter->setCollapsible(0, false);
+    right_side_panel_splitter->setCollapsible(1, false);
+    right_side_panel_splitter->setCollapsible(2, false);
+    right_side_panel_splitter->setCollapsible(3, false);
+    right_side_panel_splitter->setCollapsible(4, false);
+
+    // Main splitter
+    QSplitter *main_splitter = new QSplitter(Qt::Horizontal, this);
+    main_splitter-> addWidget(center_splitter);
+    main_splitter->addWidget(right_side_panel_splitter);
+    main_splitter->setCollapsible(0, false);
+    main_splitter->setCollapsible(1, false);
+
+    layout->addWidget(context_tool_bar);
+    layout->addWidget(main_splitter);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
-}
-
-void MainWindow::setupCanvas()
-{
-    // Configuring centralwidget
-    QVBoxLayout *layout = new QVBoxLayout(ui->centralwidget);
-    layout->setContentsMargins(0, 0, 0, 0);
-    layout->setSpacing(0);
-
-    // Contextual toolbar
-    QWidget *tool_context_bar = new QWidget(this);
-    tool_context_bar->setFixedHeight(50);
-    tool_context_bar->setStyleSheet("background-color: gray;");
-
-    // Horizontal block
-
-    QSplitter *splitter = new QSplitter(Qt::Horizontal, this);
-    QWidget *leftPane = new QWidget();
-    QPushButton *btn_1 = new QPushButton("btn 1");
-    QPushButton *btn_2 = new QPushButton("btn 2");
-    QPushButton *btn_3 = new QPushButton("btn 3");
-    QVBoxLayout *leftLayout = new QVBoxLayout(leftPane);
-    leftLayout->addWidget(btn_1);
-    leftLayout->addWidget(btn_2);
-    leftLayout->addWidget(btn_3);
-
-    canvasScene = new CanvasScene(this);
-    canvasView = new CanvasView(this);
-    canvasView->setScene(canvasScene);
-    canvasView->setDragMode(QGraphicsView::NoDrag);
-    canvasLayer = new CanvasLayerItem(128, 128);
-    canvasScene->addItem(canvasLayer);
-
-    splitter->addWidget(leftPane);
-    splitter->addWidget(canvasView);
-
-    connect(canvasScene, &CanvasScene::pixelDrawn, this, [=](int x, int y) {
-        canvasLayer->drawPixel(x, y, Qt::black);
-    });
-
-    layout->addWidget(tool_context_bar);
-    layout->addWidget(splitter);
-
-
-    //draw_frame = new QWidget(this);
-
-    //canvasView->setParent(ui->frame);
-    //canvasView->setGeometry(ui->frame->rect());
-    //canvasView->show();
-
-//    QSplitter *splitter = new QSplitter(Qt::Horizontal, this);
-//
-//    QWidget *leftPane = new QWidget();
-//    QVBoxLayout *leftLayout = new QVBoxLayout(leftPane);
-//    leftLayout->addWidget(ui->pushButton);
-//    leftLayout->addWidget(ui->pushButton_2);
-//
-//    CanvasView *canvasView = new CanvasView(this);
-//    canvasView->setScene(canvasScene);
-//
-//    splitter->addWidget(leftPane);     // Lado esquerdo (botões)
-//    splitter->addWidget(canvasView);   // Lado direito (canvas)
-//
-//    setCentralWidget(splitter);
-
-    layout->addStretch();
 
 }
 
